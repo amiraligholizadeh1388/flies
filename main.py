d421e7f26df289d4f16a3a19078b9dfef045c3fc
amirali <behrouz.gholizadeh98@gmail.com>
2024-07-12 20:40:29 +0330

    added some change for site

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-#a->b b->c c-d
-cities = ['a' , 'b' , 'c' , 'd' , 'e' , 'f']
-flies = ['a->b' , 'b->c' , 'c->d' , 'd->e' , 'e->f']
-def check_fly(mabda , maghsad , city , fly):
-    mabdaha = [i[0] for i in flies]
-    maghsadha = [i[-1] for i in flies]
-    if mabda in mabdaha:
-        if maghsad in maghsadha:
-            if f'{mabda}->{maghsad}' in flies:
-                return f'{mabda}->{maghsad}'
-            else:
-                
-                his = [mabda]
-                a = mabda
-                while a != maghsad:
-                    a = maghsadha[mabdaha.index(a)]
-                    if a not in his:
-                        his.append(a)
-                    else:
-                        his = his[0 : his.index(a)+1]
-                ticket = str(his)
-                ticket = ticket.replace('[' , '')
-                ticket = ticket.replace(']' , '')
-                ticket = ticket.replace(',' , '->')
-                ticket = ticket.replace(' ' , '')
-                ticket = ticket.replace("'" , '')
-                return ticket
-        elif maghsad not in maghsadha:
+from flask import Flask
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+if False:
+    cities = ['a' , 'b' , 'c' , 'd' , 'e' , 'f']
+    flies = ['a->b' , 'b->c' , 'c->d' , 'd->e' , 'e->f']
+    def check_fly(mabda , maghsad , city , fly):
+        mabdaha = [i[0] for i in flies]
+        maghsadha = [i[-1] for i in flies]
+        if mabda in mabdaha:
+            if maghsad in maghsadha:
+                if f'{mabda}->{maghsad}' in flies:
+                    return f'{mabda}->{maghsad}'
+                else:
+                    
+                    his = [mabda]
+                    a = mabda
+                    while a != maghsad:
+                        a = maghsadha[mabdaha.index(a)]
+                        if a not in his:
+                            his.append(a)
+                        else:
+                            his = his[0 : his.index(a)+1]
+                    ticket = str(his)
+                    ticket = ticket.replace('[' , '')
+                    ticket = ticket.replace(']' , '')
+                    ticket = ticket.replace(',' , '->')
+                    ticket = ticket.replace(' ' , '')
+                    ticket = ticket.replace("'" , '')
+                    return ticket
+            elif maghsad not in maghsadha:
+                return 'ma hamchin parvazi nadarim'
+        elif mabda not in mabdaha:
             return 'ma hamchin parvazi nadarim'
-    elif mabda not in mabdaha:
-        return 'ma hamchin parvazi nadarim'
-print(check_fly('a' , 'd' , cities , flies))
+    check_fly('a' , 'd' , cities , flies)
