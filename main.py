37fbab6327f506404964b7a98b3615dcd9780918
amirali <behrouz.gholizadeh98@gmail.com>
2024-07-13 15:30:03 +0330

    adding some files

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run()
 if False:
